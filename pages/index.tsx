5b73a0cfcb2df164b027017da9396ed0c7df0236
Atuuh <not-another-one@hotmail.com>
2020-08-23 12:28:37 +0100

    event target fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     };
 
     const onDragStart = (e: React.DragEvent<HTMLImageElement>) => {
-        dragUrl.current = e.target.src;
+        dragUrl.current = e.currentTarget.src;
     };
 
     return (
